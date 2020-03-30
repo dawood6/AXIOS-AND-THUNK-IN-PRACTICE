dc3c4796a52fc74242df43dc19568ddff5aaf879
dawood6 <dawoodsadiq171@gmail.com>
2020-03-31 03:56:49 +0500

    Feat: Axios and Thunk In React-Redux

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ class App extends Component {
       <div>
           <h1>Using Axios And Thunk In REACT-REDUX</h1>
           <h2>Using JSONPLACEHOLDER API</h2>
+          <marquee>Thanks TO Sir Osama, Sir Umair, Sir Faraz, Sir Irfan</marquee>
           {posts.map((item) => {
             return <h3>UserId:&nbsp;&nbsp;{item.userId} <br /> Id:&nbsp;&nbsp;{item.id} <br /> Title:&nbsp;&nbsp;{item.title} <br /> Body:&nbsp;&nbsp;{item.body}</h3>
           })}
